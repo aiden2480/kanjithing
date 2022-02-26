56393e2a0e0905d14eb0425443c518264655d05a
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2022-02-26 19:51:00 +1100

    Create basic index page, higher quality icons

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// (async () => {
+//     var resp = await fetch("https://api.github.com/repos/aiden2480/kanjithing/releases");
+//     var data = await resp.json();
+    
+//     data.forEach((element, index) => {
+//         console.log(index, element.zipball_url);
+//     });
+//     console.log(data);
+// })();
