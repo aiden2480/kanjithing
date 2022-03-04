3b0bec43138bdda7adc2d37342b957012ecd52a8
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2022-03-04 12:46:41 +1100

    Create default sets if not exist

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
 /* Save the current kanji */
 var current;
+var wakattaunits = [
+    "学校名前父母生高姉妹兄弟住所色",
+    "好同手紙英語何年私友行毎教場",
+    "早新家入出思来島午後朝夜牛魚族",
+    "会社持待道近町番屋店駅神様区",
+    "時間国先長話見言休聞今食勉強",
+    "帰買電車左右目口書物飲肉昼乗",
+    "曜気分多少元半使天病心楽方作文",
+    "週夏立自赤外西川旅州晩洗持活去",
+    "正冬着安広海古寺東京都北市県",
+    "森山知雪雨字読急洋服動止院漢和",
+    "春秋花南田売耳青白仕事銀犬飯",
+    "林黒羊地夕次体発馬才鳥茶歩鉄"
+];
+
 
 /* Set up a listener so we can receive messages from the console */
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
@@ -21,6 +36,23 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
 chrome.runtime.onInstalled.addListener(reason => {
     console.log("Install event fired with", reason);
     chrome.runtime.setUninstallURL("https://kanjithing-backend.chocolatejade42.repl.co/uninstall");
+
+    // Create default sets
+    chrome.storage.local.get("customsets", (result) => {
+        if (result.customsets === undefined) {
+            // {id: ..., name: ..., kanji: ..., enabled: ...}
+
+            var names = ["one", "two", "three", "four", "five", "six",
+                         "seven", "eight", "nine", "ten", "eleven", "twelve",
+            ];
+            
+            var transformed = wakattaunits.map((item, index) => {
+                return {id: index, name: "Unit " + names[index], enabled: true, kanji: item}
+            });
+
+            chrome.storage.local.set({customsets: transformed});
+        }
+    });
 });
 
 /* Script to change the browser icon */
