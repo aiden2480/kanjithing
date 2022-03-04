b2ccb1e384b0a87d6738a8f8f7c095def9ad75c3
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2022-03-04 20:06:39 +1100

    Use async chrome storage functions

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,11 @@
-chrome.storage.local.get("customsets", result => {
-    var container = document.getElementById("unitscontainer");
+const KANJI_REGEX = /^[\u4E00-\u9FAF]+$/;
 
-    // Load each unit into the container
-    result.customsets.forEach(item => {
+async function generateKanjiSets() {
+    var container = document.getElementById("setscontainer");
+    var sets = (await chrome.storage.local.get("customsets")).customsets;
+    container.innerHTML = null;
+
+    sets.forEach(item => {
         var div = document.createElement("div");
         var input = document.createElement("input");
         var bold = document.createElement("b");
@@ -17,76 +20,73 @@ chrome.storage.local.get("customsets", result => {
         div.appendChild(input);
         div.appendChild(bold);
         div.appendChild(span);
-        container.appendChild(div);
+        container.appendChild(div);    
     });
 
     // Attach event listners to each checkbox
     [...container.getElementsByTagName("input")].forEach(item => {
-        item.addEventListener("change", (event) => {
-            chrome.storage.local.get("customsets", result => {
-                var name = event.path[1].children[1].innerHTML;
-                var sets = result.customsets;
+        item.addEventListener("change", async (event) => {
+            var sets = (await chrome.storage.local.get("customsets")).customsets;
+            var name = event.path[1].children[1].innerHTML;
 
-                sets.find(i => i.name === name).enabled = event.target.checked;
-                chrome.storage.local.set({ customsets: sets });
-            })
+            sets.find(i => i.name === name).enabled = event.target.checked;
+            chrome.storage.local.set({ customsets: sets });
         });
     });
-});
+}
 
-function createSet(name, kanji) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get("customsets", result => {
-            var sets = result.customsets;
-            var id = result.customsets.slice(-1)[0].id + 1;
+function createSet(name, setstr) {
+    return new Promise(async (resolve, reject) => {
+        var sets = (await chrome.storage.local.get("customsets")).customsets;
+        var id = sets.slice(-1)[0].id + 1;
+        var kanji = setstr.replace(" ", "");
 
-            sets.push({ id, name, kanji, enabled: true });
-            chrome.storage.local.set({customsets: sets}, resolve);
-        });
+        // Ensure set is correct
+        if (!KANJI_REGEX.test(kanji)) return reject("Invalid kanji string");
+
+        sets.push({ id, name, kanji, enabled: true });
+        chrome.storage.local.set({customsets: sets}, resolve);
     });
 }
 
 function deleteSet(id) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get("customsets", result => {
-            var sets = result.customsets;
-            var updated = sets.filter(value => value.id != id);
+    return new Promise(async (resolve, reject) => {
+        var sets = (await chrome.storage.local.get("customsets")).customsets;
+        var updated = sets.filter(value => value.id != id);
 
-            chrome.storage.local.set({ customsets: updated }, resolve);
-        });
+        chrome.storage.local.set({ customsets: updated }, resolve);
     });
 }
 
 function renameSet(id, name) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get("customsets", result => {
-            var sets = result.customsets;
-            var target = sets.find(x => x.id == id);
-            var index = sets.indexOf(target);
-            
-            if (target == undefined) return reject(`Set with ID ${id} does not exist`);
+    return new Promise(async (resolve, reject) => {
+        var sets = (await chrome.storage.local.get("customsets")).customsets;
+        var target = sets.find(x => x.id == id);
+        var index = sets.indexOf(target);
+        
+        if (target == undefined) return reject(`Set with ID ${id} does not exist`);
 
-            target.name = name;
-            sets[index] = target;
+        target.name = name;
+        sets[index] = target;
 
-            chrome.storage.local.set({ customsets: sets }, resolve);
-        });
+        chrome.storage.local.set({ customsets: sets }, resolve);
     });
 }
 
 function editSetKanji(id, kanji) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get("customsets", result => {
-            var sets = result.customsets;
-            var target = sets.find(x => x.id == id);
-            var index = sets.indexOf(target);
-            
-            if (target == undefined) return reject(`Set with ID ${id} does not exist`);
+    return new Promise(async (resolve, reject) => {
+        var sets = (await chrome.storage.local.get("customsets")).customsets;
+        var target = sets.find(x => x.id == id);
+        var index = sets.indexOf(target);
+        
+        if (target == undefined) return reject(`Set with ID ${id} does not exist`);
 
-            target.kanji = kanji;
-            sets[index] = target;
+        target.kanji = kanji;
+        sets[index] = target;
 
-            chrome.storage.local.set({ customsets: sets }, resolve);
-        });
+        chrome.storage.local.set({ customsets: sets }, resolve);
     });
 }
+
+// Create kanji set nodes
+generateKanjiSets();
